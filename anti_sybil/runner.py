e0f8c2b4c21f2b0d959655e76416878b1fa1217c
Abram <abramsymons@protonmail.com>
2018-10-02 14:30:30 +0330

    Add group graph to database

--- anti_sybil/runner.py
+++ anti_sybil/runner.py
@@ -14,12 +14,27 @@ def save(graph):
         for group in node.groups:
             if not db['groups'].get({'_key': group}):
                 if group.find('seed_group_') != -1:
-                     db['groups'].insert({'_key': group, 'seed': True})
+                    db['groups'].insert({'_key': group, 'seed': True})
                 else:
                     db['groups'].insert({'_key': group})
-            db['usersInGroups'].insert({'_from': 'users/{0}'.format(node.name), '_to': 'groups/{0}'.format(group)})
+            db['usersInGroups'].insert(
+                {'_from': 'users/{0}'.format(node.name), '_to': 'groups/{0}'.format(group)})
     for edge in graph.edges():
-        db['connections'].insert({'_from': 'users/{0}'.format(edge[0].name), '_to': 'users/{0}'.format(edge[1].name)})
+        db['connections'].insert(
+            {'_from': 'users/{0}'.format(edge[0].name), '_to': 'users/{0}'.format(edge[1].name)})
+
+
+def label_seed_groups(input_file, overwrite=True):
+    client = ArangoClient()
+    db = client.db(DB_NAME, username=DB_USER, password=DB_PASS)
+    groups = db.collection('groups')
+    with open(input_file, 'rb') as f:
+        seeds = f.read().strip().split('\n')
+    for group in groups:
+        if group['_key'] in seeds:
+            db['groups'].update({'_key': group['_key'], 'seed': True})
+        elif overwrite:
+            db['groups'].update({'_key': group['_key'], 'seed': False})
 
 
 def update(nodes_graph, groups_graph):
@@ -29,7 +44,13 @@ def update(nodes_graph, groups_graph):
         db['users'].update({'_key': node.name, 'rank': node.rank})
     for group in groups_graph.nodes:
         db['groups'].update({'_key': group.name, 'rank': group.rank})
-    return True
+    for edge in groups_graph.edges.data():
+        try:
+            db['affinity'].update(
+                {'_key': '{0}-{1}'.format(edge[0], edge[1]), 'weight': edge[2]['weight']})
+        except:
+            db['affinity'].insert({'_key': '{0}-{1}'.format(edge[0], edge[1]), '_from': 'groups/{0}'.format(
+                edge[0]), '_to': 'groups/{0}'.format(edge[1]), 'weight': edge[2]['weight']})
 
 
 def load():
@@ -40,19 +61,45 @@ def load():
     connections = db.collection('connections')
     usersInGroups = db.collection('usersInGroups')
     user_groups = {}
-    seed_groups = set([group['_key'] for group in groups if group.get('seed', None)])
+    seed_groups = set([group['_key']
+                       for group in groups if group.get('seed', None)])
     for user_group in usersInGroups:
         if not user_group['_from'].replace('users/', '') in user_groups:
             user_groups[user_group['_from'].replace('users/', '')] = set()
-        user_groups[user_group['_from'].replace('users/', '')].add(user_group['_to'].replace('groups/', ''))
+        user_groups[user_group['_from'].replace(
+            'users/', '')].add(user_group['_to'].replace('groups/', ''))
     users_dic = {}
     for user in users:
-        user_type = 'Seed' if user_groups[user['_key']] & seed_groups else 'Honest'
-        users_dic[user['_key']] = graphs.node.Node(user['_key'], user_type, user_groups[user['_key']], user['rank'])
+        user_type = 'Seed' if user_groups[user['_key']
+                                          ] & seed_groups else 'Honest'
+        users_dic[user['_key']] = graphs.node.Node(
+            user['_key'], user_type, user_groups[user['_key']], user['rank'])
     edges = [(users_dic[connection['_from'].replace('users/', '')], users_dic[connection['_to'].replace('users/', '')])
-        for connection in connections]
+             for connection in connections]
     graph = nx.Graph()
     graph.add_edges_from([(edge[0], edge[1]) for edge in edges])
+    return graph
+
+
+def load_group_graph():
+    client = ArangoClient()
+    db = client.db(DB_NAME, username=DB_USER, password=DB_PASS)
+    groups = db.collection('groups')
+    group_connections = db.collection('affinity')
+    edges = []
+    i = 0
+    group_dic = {}
+    for group in groups:
+        group_dic[group['_key']] = graphs.node.Node(
+            group['_key'], 'Seed' if group['seed'] else 'Honest', [], group['rank'])
+    for connection in group_connections:
+        edges.append((
+            group_dic[connection['_from'].replace('groups/', '')],
+            group_dic[connection['_to'].replace('groups/', '')],
+            {'weight': connection['weight']}
+        ))
+    graph = nx.Graph()
+    graph.add_edges_from(edges)
     return graph
 
 
@@ -65,9 +112,10 @@ def clear():
 
 
 if __name__ == '__main__':
-    # clear()
-    # graph = load_graph('simulation_platform/graph.json')
-    # save(graph)
+    clear()
+    graph = load_graph('simulation_platform/graph.json')
+    save(graph)
+    label_seed_groups('./seed_groups.txt')
     graph = load()
     reset_ranks(graph)
     ranker = algorithms.SybilGroupRank(graph, {
@@ -78,5 +126,6 @@ if __name__ == '__main__':
     })
     ranker.rank()
     draw_graph(ranker.graph, 'nodes.html')
-    draw_graph(ranker.group_graph, 'groups.html')
     update(ranker.graph, ranker.group_graph)
+    group_graph = load_group_graph()
+    draw_graph(group_graph, 'groups.html')
