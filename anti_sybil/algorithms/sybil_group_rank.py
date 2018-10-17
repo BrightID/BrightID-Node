d7ddd416e1ce11b3edab0d6c65f0003361e85119
Abram Symons <abramsymons@protonmail.com>
2018-10-17 20:49:17 +0330

    Update sybil_group_rank.py to consider inconsistency and min neighbours in detecting attacks

--- anti_sybil/algorithms/sybil_group_rank.py
+++ anti_sybil/algorithms/sybil_group_rank.py
@@ -20,18 +20,34 @@ class SybilGroupRank(sybil_rank.SybilRank):
     def rank(self):
         ranker = sybil_rank.SybilRank(self.group_graph, self.options)
         ranker.rank()
-        node_groups_rank = {}
-        for group_node in self.group_graph.nodes:
-            for node in self.groups[group_node.name]:
-                if node not in node_groups_rank:
-                    node_groups_rank[node] = []
-                node_groups_rank[node].append(group_node.rank)
+        groups_ranks = {g.name: (g.raw_rank, g.rank) for g in self.group_graph.nodes}
         for node in self.graph.nodes:
-            if node in node_groups_rank:
-                node.rank = max(node_groups_rank[node])
-            else:
-                node.rank = 0
-                print('{} not in any group!'.format(node))
+            max_group = max(node.groups, key=lambda g: groups_ranks[g][0])
+            node.raw_rank, node.rank = groups_ranks[max_group]
+        if 'weaken_inconsistency_ratio' in self.options:
+            self.set_inconsistency()
+            sorted_groups = sorted([group for group in self.group_graph.nodes], key=lambda g: g.inconsistency, reverse=True)
+            index = int(len(sorted_groups) * self.options['weaken_inconsistency_ratio'])
+            border = sorted_groups[index].inconsistency
+            for group in self.group_graph:
+                if group.inconsistency > border:
+                    group.raw_rank = group.raw_rank / (group.inconsistency / border) ** 2
+        if 'min_neighborhood_factor' in self.options:
+            self.set_neighborhood_factor()
+            sorted_groups = sorted([group for group in self.group_graph.nodes], key=lambda g: g.neighborhood_factor)
+            border = self.options['min_neighborhood_factor']
+            for group in self.group_graph:
+                if group.neighborhood_factor < border:
+                    group.raw_rank = group.raw_rank * (group.neighborhood_factor / border)
+        if 'min_neighborhood_factor' in self.options or if 'weaken_inconsistency_ratio' in self.options:
+            ranks = dict((group, group.raw_rank) for group in self.group_graph.nodes)
+            ranked_trust = dict(ranker.normalize_nodes_rank(ranks))
+            for group in self.group_graph.nodes:
+                group.rank = ranked_trust[group]
+            groups_ranks = {g.name: (g.raw_rank, g.rank) for g in self.group_graph.nodes}
+            for node in self.graph.nodes:
+                max_group = max(node.groups, key=lambda g: groups_ranks[g][0])
+                node.raw_rank, node.rank = groups_ranks[max_group]
         return self.group_graph
 
     def get_group_type(self, group_nodes):
@@ -49,6 +65,7 @@ class SybilGroupRank(sybil_rank.SybilRank):
         groups_dic = dict([(group, Node(group, self.get_group_type(self.groups[group]))) for group in self.groups])
         pairs = itertools.combinations(self.groups.keys(), 2)
         pairs = sorted([(f, t) if f < t else (t, f) for f, t in pairs], key=lambda pair: str(pair))
+        conn_ratio_dic = {}
         for source_group, target_group in pairs:
             removed = set()
             weight = 0
@@ -67,8 +84,50 @@ class SybilGroupRank(sybil_rank.SybilRank):
                     removed.add(source_node)
                     removed.add(target_node)
                     weight += 1
-
             if weight > 0:
                 num = len(self.groups[source_group]) + len(self.groups[target_group])
                 group_graph.add_edge(groups_dic[source_group], groups_dic[target_group], weight=1.0*weight/num)
         return group_graph
+
+    def set_inconsistency(self):
+        groups_dict = {group_object.name: group_object for group_object in self.group_graph.nodes}
+        for group in self.groups:
+            nodes = sorted(self.groups[group], key=lambda n: n.name)
+            node_neighbors_dict = {}
+            for node in nodes:
+                node_neighbors_dict[node] = set()
+                for neighbor in self.graph.neighbors(node):
+                    if not group in neighbor.groups:
+                        node_neighbors_dict[node].update(neighbor.groups)
+            nodes_activity = [sum([groups_dict[g].raw_rank for g in node_neighbors_dict[node]]) for node in nodes]
+            avg_activity = sum(nodes_activity) / len(nodes_activity)
+            actives = [activity for activity in nodes_activity if activity >= avg_activity]
+            inactives = [activity for activity in nodes_activity if activity <= avg_activity]
+            # It should never happens but in some situations takes place because of floating point problems.
+            if len(actives) == 0 or len(inactives) == 0:
+                groups_dict[group].inconsistency = 0
+                continue
+            avg_actives = sum(actives) / len(actives)
+            avg_inactives = sum(inactives) / len(inactives)
+            if avg_inactives > 0:
+                inconsistency = avg_actives / avg_inactives
+            else:
+                inconsistency = None
+            multiplier = (float(len(inactives)) / len(actives)) ** .5
+            if multiplier > 1 and inconsistency != None:
+                inconsistency *= multiplier
+            groups_dict[group].inconsistency = inconsistency
+        max_inconsistencies = max([node.inconsistency for node in self.group_graph.nodes])
+        for group in self.groups:
+            if groups_dict[group].inconsistency == None:
+                groups_dict[group].inconsistency = max_inconsistencies
+
+    def set_neighborhood_factor(self):
+        for group in self.group_graph:
+            neighboring_nodes = set()
+            nodes = sorted(self.groups[group.name], key=lambda n: n.name)
+            for node in nodes:
+                for neighbor in self.graph.neighbors(node):
+                    if not group.name in neighbor.groups and neighbor.rank >= self.options['min_reliable_rank']:
+                        neighboring_nodes.add(neighbor)
+            group.neighborhood_factor = min(len(neighboring_nodes), self.group_graph.degree(group))
---
+++ anti_sybil/tests/heavy_target_attack.py
@@ -0,0 +1,81 @@
+import sys
+import random
+sys.path.append('..')
+
+import algorithms
+from graphs.node import Node
+from utils import *
+from collections import OrderedDict
+
+OUTPUT_FOLDER = './outputs/sparse_graph_target_attack/'
+
+graph_params = {
+    'num_seed_nodes': 30,
+    'num_sybil_to_num_honest': 0,
+    'sybil_con_to_honest_con': 0.5,
+    'num_bridge_to_num_non_bridge': 1,
+    'num_attacker_to_num_honest': 0,
+    'num_sybil_to_num_attacker': 1,
+    'bridge_to_attackers_con': 0.3,
+    'num_honest': 47,
+    'min_degree': 10,
+    'max_degree': 25,
+    'avg_degree': 15,
+    'num_groups': 100,
+    'min_group_nodes': 3,
+    'max_group_nodes': 10,
+    'num_joint_node': 300,
+    'num_seed_groups': 5,
+    'min_known_ratio': .125,
+    'avg_known_ratio': .5,
+    'max_known_ratio': 1,
+    'sybil_to_attackers_con': .5,
+    'num_inter_group_con': 300,
+}
+
+algorithm_options = {
+    'accumulative': False,
+    'weaken_under_min': False,
+    'min_degree': 5,
+    'nonlinear_distribution': True,
+    'group_edge_weight': 20,
+    'weaken_inconsistency_ratio': .1,
+    'min_neighborhood_factor': 5,
+    'min_reliable_rank': 20,
+    'thresholds': [.36, .24, .18, .12, .06, .04, .02, .01, .005, .004, .003, .002, .0015, .001, .0005, 0]
+}
+
+NUM_ATTACKERS = 5
+NUM_SYBILS = 10
+TOP = 30
+
+def attack(graph):
+    ranker = algorithms.SybilGroupRank(graph, algorithm_options)
+    ranker.rank()
+    edges = []
+    attackers = []
+    outputs = []
+    nodes = sorted(list(graph.nodes), key=lambda n: n.rank, reverse=True)
+    nodes_dic = OrderedDict([(node.name, node) for node in nodes])
+    for i in range(NUM_ATTACKERS):
+        nodes_dic['a-{0}'.format(i)] = Node('a-{0}'.format(i), 'Attacker', groups=set(['target_attack']))
+        attackers.append('a-{0}'.format(i))
+    for top_node in nodes[:TOP]:
+        edges.append((nodes_dic[random.choice(attackers)], nodes_dic[top_node.name]))                
+    for i in range(NUM_SYBILS):
+        nodes_dic['s-{0}'.format(i)] = Node('s-{0}'.format(i), 'Sybil', groups=set(['target_attack']))
+        for attacker in attackers:
+            edges.append((nodes_dic['s-{0}'.format(i)], nodes_dic[attacker]))                
+    graph.add_edges_from(edges)
+    reset_ranks(graph)
+    ranker = algorithms.SybilGroupRank(graph, algorithm_options)
+    ranker.rank()
+    outputs.append(generate_output(graph))
+    draw_graph(graph, os.path.join(OUTPUT_FOLDER, 'SybilGroupRank.html'))
+    draw_graph(ranker.group_graph, os.path.join(OUTPUT_FOLDER, 'SybilGroupRank2.html'))
+    write_output_file(outputs, os.path.join(OUTPUT_FOLDER, 'result.csv'))
+
+
+if __name__ == '__main__':
+    graph = graphs.generators.group_based.generate(graph_params)
+    attack(graph)
