0527731109bee12207aa930a17a1acfdc871d28a
adamstallard <adam.stallard@gmail.com>
2018-12-14 12:04:19 -0800

    don't use authentication for arangodb

--- web_services/docker-entrypoint.sh
+++ web_services/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -eo pipefail
 
 /wait-for.sh localhost:8529
-foxx install /brightid /code/foxx/brightid_1.0.0.zip -p /code/pass || true
-foxx config /brightid ip=`curl https://ipinfo.io/ip` -p /code/pass || true
+foxx install /brightid /code/foxx/brightid_1.0.0.zip || true
+foxx config /brightid ip=`curl https://ipinfo.io/ip` || true
 
 exit 0
--- web_services/foxx/brightid_1.0.0/deploy.sh
+++ web_services/foxx/brightid_1.0.0/deploy.sh
@@ -1,4 +1,4 @@
 rm -rf ../brightid_1.0.0.zip
 zip -r ../brightid_1.0.0.zip .
-foxx uninstall /brightid -p ./pass
-foxx install /brightid ../brightid_1.0.0.zip -p ./pass
+foxx uninstall /brightid
+foxx install /brightid ../brightid_1.0.0.zip
