3e5714f32ec08cdc00b5fcfbc4992ff8e965b877
adamstallard <adam.stallard@gmail.com>
2018-12-28 03:21:47 -0800

    need to compare just keys now due to change in return value from userConnections

--- web_services/foxx/brightid_1.0.0/db.js
+++ web_services/foxx/brightid_1.0.0/db.js
@@ -275,7 +275,7 @@ function createGroup(key1, key2, key3, timestamp) {
     throw 'Duplicate group';
   }
 
-  const conns = userConnections(user1);
+  const conns = userConnections(key1);
 
   if (conns.indexOf(key2) < 0 || conns.indexOf(key3) < 0) {
     throw "Creator isn't connected to one or both of the co-founders";
