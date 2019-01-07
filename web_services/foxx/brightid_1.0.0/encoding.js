4bf342c131c5ef203d804cf8ce0cf956595ff939
adamstallard <adam.stallard@gmail.com>
2019-01-07 01:39:31 -0800

    fix bug with uint8array conversion. add tests.

--- web_services/foxx/brightid_1.0.0/encoding.js
+++ web_services/foxx/brightid_1.0.0/encoding.js
@@ -2,12 +2,12 @@
 const B64 = require('base64-js');
 
 function b64ToUint8Array(str) {
-  return Object.values(B64.toByteArray(str));
+  return new Uint8Array(B64.toByteArray(str));
 }
 
 function strToUint8Array(str) {
   const b = Buffer.from(str);
-  return b.slice(b.byteOffset, b.byteOffset + b.byteLength);
+  return new Uint8Array(b.slice(b.byteOffset, b.byteOffset + b.byteLength));
 }
 
 function b64ToUrlSafeB64(s) {
