e8222c23fb2b040af6cf09611bd261101b3a2257
adamstallard <adam.stallard@gmail.com>
2019-09-10 00:17:37 -0700

    use 'upgrade' when possible to keep existing configuration

--- web_services/docker-entrypoint.sh
+++ web_services/docker-entrypoint.sh
@@ -2,8 +2,6 @@
 set -eo pipefail
 
 /wait-for.sh localhost:8529
-foxx uninstall /brightid
+foxx upgrade /brightid /code/foxx/brightid_1.0.0.zip ||
 foxx install /brightid /code/foxx/brightid_1.0.0.zip
 foxx config /brightid ip=`curl https://ipinfo.io/ip`
-
-exit 0
