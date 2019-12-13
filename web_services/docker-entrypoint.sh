ef87cb5ab02d72437275ed269186d266bb82b90d
Abram Symons <abramsymons@gmail.com>
2019-12-13 11:53:08 -0700

    upgrade to v 2.0.0

--- web_services/docker-entrypoint.sh
+++ web_services/docker-entrypoint.sh
@@ -2,6 +2,6 @@
 set -eo pipefail
 
 /wait-for.sh localhost:8529
-foxx upgrade /brightid /code/foxx/brightid_1.0.0.zip ||
-foxx install /brightid /code/foxx/brightid_1.0.0.zip
+foxx upgrade /brightid /code/foxx/brightid_2.0.0.zip ||
+foxx install /brightid /code/foxx/brightid_2.0.0.zip
 foxx config /brightid ip=`curl https://ipinfo.io/ip`
