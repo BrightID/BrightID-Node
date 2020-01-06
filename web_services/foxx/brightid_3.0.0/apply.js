476aebe599a764fe707c42b16c322d72d149cebf
abramsymons <abramsymons@protonmail.com>
2020-01-06 05:35:58 -0500

    add missed files from previous commit

---
+++ web_services/foxx/brightid_3.0.0/apply.js
@@ -0,0 +1,45 @@
+'use strict';
+const createRouter = require('@arangodb/foxx/router');
+const joi = require('joi');
+const arango = require('@arangodb').db;
+const nacl = require('tweetnacl');
+const db = require('./db');
+const operations = require('./operations');
+
+const router = createRouter();
+module.context.use(router);
+const operationsHashesColl = arango._collection('operationsHashes');
+
+const handlers = {
+  operationsPut: function(req, res){
+    const op = req.body;
+    
+    if (operationsHashesColl.exists(op._key)) {
+      return res.send({'success': true, 'state': 'duplicate'});
+    }
+    operationsHashesColl.insert({ _key: op._key });
+
+    if (op.name == 'Verify Account') {
+      operations.decrypt(op);
+    }
+    try {
+      operations.verify(op);
+      op.result = operations.apply(op);
+      op.state = 'applied';
+    } catch (e) {
+      op.state = 'failed';
+      op.result = e + (e.stack ? '\n' + e.stack : '');
+    }
+    if (op.name == 'Verify Account') {
+      operations.encrypt(op);
+    }
+    db.upsertOperation(op);
+    res.send({'success': true, 'state': op.state, 'result': op.result});
+  }
+};
+
+router.put('/operations', handlers.operationsPut)
+  .body(joi.object().required())
+  .summary('Apply operation after consensus')
+  .description("Apply operation after consensus.")
+  .response(null);
