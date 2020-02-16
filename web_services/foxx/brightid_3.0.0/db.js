8968d8da84c4162b02694f4a897f42a228e17c60
Abram Symons <abramsymons@protonmail.com>
2020-02-16 13:24:25 +0330

    not use spread notation

--- web_services/foxx/brightid_3.0.0/db.js
+++ web_services/foxx/brightid_3.0.0/db.js
@@ -50,14 +50,12 @@ function addConnection(key1, key2, timestamp){
 
   // remove flag if exists
   if (u1.flaggers && key2 in u1.flaggers) {
-    const flaggers = {...u1.flaggers};
-    delete flaggers[key2];
-    usersColl.update(u1, { flaggers }, { mergeObjects: false });
+    delete u1.flaggers[key2];
+    usersColl.update(u1, { flaggers: u1.flaggers }, { mergeObjects: false });
   }
   if (u2.flaggers && key1 in u2.flaggers) {
-    const flaggers = {...u2.flaggers};
-    delete flaggers[key1];
-    usersColl.update(u2, { flaggers }, { mergeObjects: false });
+    delete u2.flaggers[key1];
+    usersColl.update(u2, { flaggers: u2.flaggers }, { mergeObjects: false });
   }
 
   const conn = getConnection(key1, key2);
