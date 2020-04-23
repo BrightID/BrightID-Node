de2df6790fa128b386ddb984efe1a512a3a8085c
Siftal <m.khanmohamadzadeh@gmail.com>
2020-04-23 20:43:13 +0900

    fix a typo

--- scorer/runner.py
+++ scorer/runner.py
@@ -64,7 +64,7 @@ def stupid_sybil_border(graph):
 
 
 def process(fname):
-    json_graph = from_dump(f)
+    json_graph = from_dump(fname)
     graph = from_json(json_graph)
     border = stupid_sybil_border(graph)
     raw_ranks = [node.raw_rank for node in graph.nodes]
