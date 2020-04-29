f775d7827e68390c464f105ebf5c6823a9f61743
Siftal <m.khanmohamadzadeh@gmail.com>
2020-04-29 15:59:17 +0900

    update the errors

--- web_services/foxx/brightid_4.0.0/index.js
+++ web_services/foxx/brightid_4.0.0/index.js
@@ -24,6 +24,19 @@ const operationsHashesColl = arango._collection('operationsHashes');
 
 const TIME_FUDGE = 60 * 60 * 1000; // timestamp can be this far in the future (milliseconds) to accommodate client/server clock differences
 
+// error numbers
+const CONTEXT_NOT_FOUND = 1;
+const CONTEXTID_NOT_FOUND = 2;
+const CAN_NOT_BE_VERIFIED = 3;
+const NOT_SPONSORED = 4;
+const OLD_ACCOUNT = 5;
+const KEYPAIR_NOT_SET = 6;
+const ETHPRIVATEKEY_NOT_SET = 7;
+const ETHNAME_NOT_SET = 8;
+const OPERATION_NOT_FOUND = 9;
+const USER_NOT_FOUND = 10;
+const IP_NOT_SET = 11;
+
 const handlers = {
   operationsPut: function(req, res){
     const op = req.body;
@@ -58,7 +71,7 @@ const handlers = {
         }
       });
     } else {
-      res.throw(404, "Operation not found");
+      res.throw(404, "Operation not found", {errorNum: OPERATION_NOT_FOUND});
     }
   },
 
@@ -67,7 +80,7 @@ const handlers = {
 
     const user = db.loadUser(id);
     if (! user) {
-      res.throw(404, "User not found");
+      res.throw(404, "User not found", {errorNum: USER_NOT_FOUND});
     }
 
     const connections = db.userConnections(id);
@@ -94,7 +107,7 @@ const handlers = {
 
     const context = db.getContext(contextName);
     if (! context) {
-      res.throw(404, 'context not found');
+      res.throw(404, 'context not found', {errorNum: CONTEXT_NOT_FOUND});
     }
 
     const coll = arango._collection(context.collection);
@@ -108,94 +121,83 @@ const handlers = {
   },
 
   verificationGet: function(req, res){
-    try {
-      const contextId = req.param('contextId');
-      let contextName = req.param('context');
-      const signed = req.param('signed');
-      const context = db.getContext(contextName);
-      if (! context) {
-        throw 'context not found';
-      }
-
-      const coll = arango._collection(context.collection);
-      const user = db.getUserByContextId(coll, contextId);
-      if (! user) {
-        throw 'contextId not found';
-      }
-
-      if (! db.isSponsored(user)) {
-        throw 'user is not sponsored';
-      }
-
-      if (! db.userHasVerification(context.verification, user)) {
-        throw 'user can not be verified for this context';
-      }
-
-      let contextIds = db.getContextIdsByUser(coll, user);
-      if (contextId != contextIds[0]) {
-        throw 'user is not using this account anymore';
-      }
-
-      // sign and return the verification
-      let sig, publicKey;
-      if (signed == 'nacl') {
-        if (! (module.context && module.context.configuration && module.context.configuration.publicKey && module.context.configuration.privateKey)){
-          throw 'Server node key pair not configured';
-        }
-
-        const message = contextName + ',' + contextIds.join(',');
-        const privateKey = module.context.configuration.privateKey;
-        publicKey = module.context.configuration.publicKey;
-        sig = uInt8ArrayToB64(
-          Object.values(nacl.sign.detached(strToUint8Array(message), b64ToUint8Array(privateKey)))
-        );
-      } else if (signed == 'eth') {
-        if (! (module.context && module.context.configuration && module.context.configuration.ethPrivateKey)){
-          throw 'Server setting "ethPrivateKey" not set';
-        }
-
-        if (!context.ethName){
-          throw `"ethName" not set for context "${contextName}"`;
-        }
-
-        contextName = context.ethName;
-        let message, h;
-        if (context.idsAsHex){
-          message = pad32(contextName) + contextIds.map(addressToBytes32).join('');
-        } else {
-          message = pad32(contextName) + contextIds.map(pad32).join('');
-        }
-        message = Buffer.from(message, 'binary').toString('hex');
-        h = new Uint8Array(createKeccakHash('keccak256').update(message, 'hex').digest());
-        let ethPrivateKey = module.context.configuration.ethPrivateKey;
-        ethPrivateKey = new Uint8Array(Buffer.from(ethPrivateKey, 'hex'));
-        publicKey = Buffer.from(Object.values(secp256k1.publicKeyCreate(ethPrivateKey))).toString('hex');
-        const _sig = secp256k1.ecdsaSign(h, ethPrivateKey);
-        sig = {
-          r: Buffer.from(Object.values(_sig.signature.slice(0, 32))).toString('hex'),
-          s: Buffer.from(Object.values(_sig.signature.slice(32, 64))).toString('hex'),
-          v: _sig.recid + 27,
-        }
-      }
-      res.send({
-        data: {
-          unique: true,
-          context: contextName,
-          contextIds: contextIds,
-          sig,
-          publicKey
-        }
-      });
-    } catch (e) {
-      res.send({
-        data: {
-          unique: false,
-          context: req.param('context'),
-          contextIds: [],
-          errorMessage: e
-        }
-      });
+    const contextId = req.param('contextId');
+    let contextName = req.param('context');
+    const signed = req.param('signed');
+    const context = db.getContext(contextName);
+    if (! context) {
+      res.throw(404, 'context not found', {errorNum: CONTEXT_NOT_FOUND});
     }
+
+    const coll = arango._collection(context.collection);
+    const user = db.getUserByContextId(coll, contextId);
+    if (! user) {
+      res.throw(404, 'contextId not found', {errorNum: CONTEXTID_NOT_FOUND});
+    }
+
+    if (! db.isSponsored(user)) {
+      res.throw(404, 'user is not sponsored', {errorNum: NOT_SPONSORED});
+    }
+
+    if (! db.userHasVerification(context.verification, user)) {
+      res.throw(404, 'user can not be verified for this context', {errorNum: CAN_NOT_BE_VERIFIED});
+    }
+
+    let contextIds = db.getContextIdsByUser(coll, user);
+    if (contextId != contextIds[0]) {
+      res.throw(404, 'user is not using this account anymore', {errorNum: OLD_ACCOUNT});
+    }
+
+    // sign and return the verification
+    let sig, publicKey;
+    if (signed == 'nacl') {
+      if (! (module.context && module.context.configuration && module.context.configuration.publicKey && module.context.configuration.privateKey)){
+        res.throw(500, 'Server setting key pair not set', {errorNum: KEYPAIR_NOT_SET});
+      }
+
+      const message = contextName + ',' + contextIds.join(',');
+      const privateKey = module.context.configuration.privateKey;
+      publicKey = module.context.configuration.publicKey;
+      sig = uInt8ArrayToB64(
+        Object.values(nacl.sign.detached(strToUint8Array(message), b64ToUint8Array(privateKey)))
+      );
+    } else if (signed == 'eth') {
+      if (! (module.context && module.context.configuration && module.context.configuration.ethPrivateKey)){
+        res.throw(500, 'Server setting "ethPrivateKey" not set', {errorNum: ETHPRIVATEKEY_NOT_SET});
+      }
+
+      if (!context.ethName) {
+        res.throw(404, `"ethName" not set for context "${contextName}"`, {errorNum: ETHNAME_NOT_SET});
+      }
+
+      contextName = context.ethName;
+      let message, h;
+      if (context.idsAsHex){
+        message = pad32(contextName) + contextIds.map(addressToBytes32).join('');
+      } else {
+        message = pad32(contextName) + contextIds.map(pad32).join('');
+      }
+      message = Buffer.from(message, 'binary').toString('hex');
+      h = new Uint8Array(createKeccakHash('keccak256').update(message, 'hex').digest());
+      let ethPrivateKey = module.context.configuration.ethPrivateKey;
+      ethPrivateKey = new Uint8Array(Buffer.from(ethPrivateKey, 'hex'));
+      publicKey = Buffer.from(Object.values(secp256k1.publicKeyCreate(ethPrivateKey))).toString('hex');
+      const _sig = secp256k1.ecdsaSign(h, ethPrivateKey);
+      sig = {
+        r: Buffer.from(Object.values(_sig.signature.slice(0, 32))).toString('hex'),
+        s: Buffer.from(Object.values(_sig.signature.slice(32, 64))).toString('hex'),
+        v: _sig.recid + 27,
+      }
+    }
+    res.send({
+      data: {
+        unique: true,
+        context: contextName,
+        contextIds: contextIds,
+        sig,
+        publicKey
+      }
+    });
   },
 
   ip: function ip(req, res){
@@ -207,15 +209,15 @@ const handlers = {
         }
       });
     } else {
-      res.throw(500, "Ip address unknown");
+      res.throw(500, 'Server setting "ip" not set', {errorNum: IP_NOT_SET});
     }
   },
 
   contexts: function contexts(req, res){
     const contextName = req.param('context');
     let context = db.getContext(contextName);
-    if (context == null) {
-      res.throw(404, 'Context not found');
+    if (! context) {
+      res.throw(404, 'Context not found', {errorNum: CONTEXT_NOT_FOUND} );
     } else {
       res.send({
         "data": {
