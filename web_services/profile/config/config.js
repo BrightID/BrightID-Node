dbeb24330e91ea4c07a8229b6d40fdb19547114b
adamstallard <adam.stallard@gmail.com>
2020-05-04 21:19:52 -0700

    Revert "update profile service TTL"
    This reverts commit b224bf49

--- web_services/profile/config/config.js
+++ web_services/profile/config/config.js
@@ -1,7 +1,7 @@
 var development = {
   port: 3000,
   node_cache: {
-    stdTTL: 7200,
+    stdTTL: 900,
     checkperiod: 120
   }
 };
@@ -9,7 +9,7 @@ var development = {
 var production = {
   port: 3000,
   node_cache: {
-    stdTTL: 7200,
+    stdTTL: 900,
     checkperiod: 120
   }
 };
