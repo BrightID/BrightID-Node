b224bf49acb2483062799f123f6d8684d95f5af6
rnbwd <dwisner6@gmail.com>
2020-05-05 11:15:16 +0800

    update profile service TTL

--- web_services/profile/config/config.js
+++ web_services/profile/config/config.js
@@ -1,7 +1,7 @@
 var development = {
   port: 3000,
   node_cache: {
-    stdTTL: 900,
+    stdTTL: 7200,
     checkperiod: 120
   }
 };
@@ -9,7 +9,7 @@ var development = {
 var production = {
   port: 3000,
   node_cache: {
-    stdTTL: 900,
+    stdTTL: 7200,
     checkperiod: 120
   }
 };
