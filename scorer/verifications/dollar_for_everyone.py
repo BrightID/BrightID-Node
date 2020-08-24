7824e36da8e80423baa819a40f4ad9c815b32b7f
Siftal <m.khanmohamadzadeh@gmail.com>
2020-08-24 16:27:51 +0430

    check the connections timestamp

--- scorer/verifications/dollar_for_everyone.py
+++ scorer/verifications/dollar_for_everyone.py
@@ -6,8 +6,8 @@ def verify():
     print('DOLLAR FOR EVERYONE')
     db = ArangoClient().db('_system')
     for admin in db['users'].find({'dfeAdmin': True}):
-        conns1 = [c['_to'] for c in db['connections'].find({'_from': admin['_id']})]
-        conns2 = [c['_from'] for c in db['connections'].find({'_to': admin['_id']})]
+        conns1 = [c['_to'] for c in db['connections'].find({'_from': admin['_id']}) if c['timestamp'] > 1564600000000]
+        conns2 = [c['_from'] for c in db['connections'].find({'_to': admin['_id']}) if c['timestamp'] > 1564600000000]
         for u in conns1 + conns2:
             verifications = set([v['name'] for v in db['verifications'].find({'user': u})])
             if 'DollarForEveryone' not in verifications:
