35ba304ef6d5489b3c62a37b6caee985915ca956
Abram Symons <abramsymons@gmail.com>
2020-09-20 17:50:35 +0430

    handle all sorts of exceptions in scorer

--- scorer/runner.py
+++ scorer/runner.py
@@ -20,8 +20,7 @@ def process(fname):
         reset_ranks(graph)
         verifier.verify(graph)
 
-
-if __name__ == '__main__':
+def main():
     variables = db.collection('variables')
     if not variables.has('VERIFICATION_BLOCK'):
         variables.insert({
@@ -39,16 +38,20 @@ if __name__ == '__main__':
         fname = os.path.join(SNAPSHOTS_PATH, snapshots[0])
         print(
             '{} - processing {} started ...'.format(str(datetime.now()).split('.')[0], fname))
+        process(fname)
+        block = int(snapshots[0].strip('dump_').strip('.zip'))
+        variables.update({'_key': 'VERIFICATION_BLOCK', 'value': block})
+        if os.path.exists(fname):
+            os.remove(fname)
+        else:
+            print(f'{fname} does not exist')
+        print(
+            '{} - processing {} completed'.format(str(datetime.now()).split('.')[0], fname))
+
+if __name__ == '__main__':
+    while True:
         try:
-            process(fname)
-            block = int(snapshots[0].strip('dump_').strip('.zip'))
-            variables.update({'_key': 'VERIFICATION_BLOCK', 'value': block})
-            if os.path.exists(fname):
-                os.remove(fname)
-            else:
-                print(f'{fname} does not exist')
-            print(
-                '{} - processing {} completed'.format(str(datetime.now()).split('.')[0], fname))
+            main()
         except Exception as e:
             print(f'Error: {e}')
             time.sleep(10)
