d388eaefae3d2f000068d81a3b7985a4a5ae2659
Siftal <m.khanmohamadzadeh@gmail.com>
2020-10-25 14:27:16 +0330

    grant Yekta_n to the user which have Yekta with rank n

--- scorer/verifications/yekta.py
+++ scorer/verifications/yekta.py
@@ -2,10 +2,14 @@ import time
 from arango import ArangoClient
 from anti_sybil.utils import draw_graph
 import anti_sybil.algorithms as algorithms
+from anti_sybil.utils import *
 
 
-def verify(graph):
-    print('Yekta')
+def verify(fname):
+    print('YEKTA')
+    json_graph = from_dump(fname)
+    graph = from_json(json_graph)
+    reset_ranks(graph)
     db = ArangoClient().db('_system')
     ranker = algorithms.Yekta(graph, {})
     ranker.rank()
@@ -37,4 +41,13 @@ def verify(graph):
             'raw_rank': node.raw_rank,
             'timestamp': int(time.time() * 1000)
         })
-    print('Yekta verifieds: ', counter)
+        verifications = set([v['name'] for v in db['verifications'].find({'user': node.name})])
+        for i in range(node.rank):
+            verification = f'Yekta_{i + 1}'
+            if verification not in verifications:
+                db['verifications'].insert({
+                    'name': verification,
+                    'user': node.name,
+                    'timestamp': int(time.time() * 1000)
+                })
+    print(f'verifieds: {counter}\n')
