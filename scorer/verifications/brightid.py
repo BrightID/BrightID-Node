f8d3dd052c4bcf79dcd68514fd5e8bbc1c76253b
Siftal <m.khanmohamadzadeh@gmail.com>
2020-10-29 15:22:52 +0330

    grant brightid verification to the users which have the seedConnected verification

--- scorer/verifications/brightid.py
+++ scorer/verifications/brightid.py
@@ -8,8 +8,7 @@ def verify(fname):
     for u in db['users']:
         verifications = set([v['name'] for v in db['verifications'].find({'user': u['_key']})])
         seedConnected = 'SeedConnected' in verifications
-        seedConnectedWithFriend = 'SeedConnectedWithFriend' in verifications
-        if not (seedConnected and seedConnectedWithFriend):
+        if not seedConnected:
             continue
         if 'BrightID' not in verifications:
             db['verifications'].insert({
