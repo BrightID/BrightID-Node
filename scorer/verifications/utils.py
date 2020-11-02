c0755e65af7907181eb144ef724b2d09f70bb17a
Siftal <m.khanmohamadzadeh@gmail.com>
2020-11-02 10:47:58 +0330

    add utils.py

---
+++ scorer/verifications/utils.py
@@ -0,0 +1,19 @@
+import zipfile
+import json
+
+
+def zip2dict(f, table):
+    zf = zipfile.ZipFile(f)
+    fnames = zf.namelist()
+    def pattern(fname): return fname.endswith(
+        '.data.json') and fname.count('/{}_'.format(table)) > 0
+    fname = list(filter(pattern, fnames))[0]
+    content = zf.open(fname).read().decode('utf-8')
+    ol = [json.loads(line) for line in content.split('\n') if line.strip()]
+    d = {}
+    for o in ol:
+        if o['type'] == 2300:
+            d[o['data']['_key']] = o['data']
+        elif o['type'] == 2302 and o['data']['_key'] in d:
+            del d[o['data']['_key']]
+    return d.values()
