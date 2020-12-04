ae6041d77896186e33c1585bb30f4e5c4021ad2d
Siftal <m.khanmohamadzadeh@gmail.com>
2020-12-04 07:33:58 -0500

    some updates, to have consensus on the verifications

--- scorer/verifications/brightid.py
+++ scorer/verifications/brightid.py
@@ -1,29 +1,29 @@
 import time
 from arango import ArangoClient
-import utils
+from .utils import documents
 
 
-def verify(fname):
+def verify(fname, past_block, current_block):
     print('BRIGHTID')
     db = ArangoClient().db('_system')
     verifications = {}
-    verifications_documents = utils.documents(fname, 'verifications')
+    verifications_documents = documents(fname, 'verifications')
     for d in verifications_documents:
         if d['user'] not in verifications:
             verifications[d['user']] = []
         verifications[d['user']].append(d['name'])
 
-    users = utils.documents(fname, 'users')
+    users = documents(fname, 'users')
 
     for u in users:
         if 'SeedConnected' not in verifications.get(u['_key'], []):
             continue
-        if 'BrightID' not in verifications[u['_key']]:
-            db['verifications'].insert({
-                'name': 'BrightID',
-                'user': u['_key'],
-                'timestamp': int(time.time() * 1000)
-            })
-            print('user: {}\tverification: BrightID'.format(u['_key']))
-    verifiedCount = db['verifications'].find({'name': 'BrightID'}).count()
-    print('verifieds: {}\n'.format(verifiedCount))
+        db['verifications'].insert({
+            'name': 'BrightID',
+            'user': u['_key'],
+            'timestamp': int(time.time() * 1000),
+            'block': current_block
+        })
+    verifiedCount = db['verifications'].find(
+        {'name': 'BrightID', 'block': current_block}).count()
+    print(f'verifieds: {verifiedCount}\n')
