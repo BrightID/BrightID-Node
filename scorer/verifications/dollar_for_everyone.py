e8987e46a5409051f7cdc0df514c69acb29a59b9
Siftal <m.khanmohamadzadeh@gmail.com>
2020-12-04 09:36:03 -0500

    some updates, to have consensus on the verifications

--- scorer/verifications/dollar_for_everyone.py
+++ scorer/verifications/dollar_for_everyone.py
@@ -1,37 +1,29 @@
 import time
 from arango import ArangoClient
-import utils
+from .utils import documents
 
 SEED_CONNECTION_LEVELS = ['just met', 'already known', 'recovery']
 
 
-def verify(fname):
+def verify(fname, past_block, current_block):
     print('DOLLAR FOR EVERYONE')
-    db = ArangoClient().db('_system')
-
-    verifications_documents = utils.documents(fname, 'verifications')
-    verifications = {}
-    for d in verifications_documents:
-        if d['user'] not in verifications:
-            verifications[d['user']] = []
-        verifications[d['user']].append(d['name'])
-
-    users_documents = utils.documents(fname, 'users')
+    users_documents = documents(fname, 'users')
     admins = filter(lambda u: u.get('dfeAdmin', False), users_documents)
     admins = [a['_id'] for a in admins]
 
-    connections_documents = utils.documents(fname, 'connections')
+    connections_documents = documents(fname, 'connections')
     connections = filter(lambda c: c['_from'] in admins and c['level']
                          in SEED_CONNECTION_LEVELS and c['timestamp'] > 1564600000000, connections_documents)
+
+    db = ArangoClient().db('_system')
     neighbors = set([c['_to'].replace('users/', '') for c in connections])
     for neighbor in neighbors:
-        if 'DollarForEveryone' not in verifications.get(neighbor, []):
-            db['verifications'].insert({
-                'name': 'DollarForEveryone',
-                'user': neighbor,
-                'timestamp': int(time.time() * 1000)
-            })
-            print('user: {}\tverification: DollarForEveryone'.format(neighbor))
+        db['verifications'].insert({
+            'name': 'DollarForEveryone',
+            'user': neighbor,
+            'timestamp': int(time.time() * 1000),
+            'block': current_block
+        })
     verifiedCount = db['verifications'].find(
-        {'name': 'DollarForEveryone'}).count()
+        {'name': 'DollarForEveryone', 'block': current_block}).count()
     print('verifieds: {}\n'.format(verifiedCount))
