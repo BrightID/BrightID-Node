68765c14553ebe5134f89f8ffa47993d2f964717
Siftal <m.khanmohamadzadeh@gmail.com>
2020-12-04 09:42:49 -0500

    some updates, to have consensus on the verifications

--- scorer/verifications/yekta.py
+++ scorer/verifications/yekta.py
@@ -1,61 +1,29 @@
 import time
-import utils
+from .utils import documents
 from arango import ArangoClient
 from anti_sybil.utils import draw_graph
 import anti_sybil.algorithms as algorithms
 from anti_sybil.utils import *
 
 
-def verify(fname):
+def verify(fname, past_block, current_block):
     print('YEKTA')
-    db = ArangoClient().db('_system')
-    verifications_documents = utils.documents(fname, 'verifications')
-    verifications = {}
-    yekta_history = {}
-    for d in verifications_documents:
-        if d['name'] == 'Yekta':
-            yekta_history[d['user']] = {'rank': d['rank'], 'raw_rank': d['raw_rank']}
-        if d['user'] not in verifications:
-            verifications[d['user']] = []
-        verifications[d['user']].append(d['name'])
-
     json_graph = from_dump(fname)
     graph = from_json(json_graph)
-    nodes_rank = {n.name: {'rank': 0, 'raw_rank': 0} for n in graph}
     ranker = algorithms.Yekta(graph, {})
     ranker.rank()
-    for node in ranker.graph:
-        nodes_rank[node.name]['rank'] = node.rank
-        nodes_rank[node.name]['raw_rank'] = node.raw_rank
     # draw_graph(ranker.graph, 'nodes.html')
-    counter = dict.fromkeys(range(0, 6), 0)
-    for n in nodes_rank:
-        counter[nodes_rank[n]['rank']] += 1
-        if nodes_rank[n] == yekta_history.get(n, None):
-            continue
-        db.aql.execute('''
-            UPSERT {
-                user: @user,
-                name: 'Yekta'
-            }
-            INSERT {
-                name: 'Yekta',
-                user: @user,
-                rank: @rank,
-                raw_rank: @raw_rank,
-                timestamp: @timestamp
-            }
-            UPDATE {
-                rank: @rank,
-                raw_rank: @raw_rank,
-                timestamp: @timestamp
-            }
-            IN verifications
-        ''', bind_vars={
-            'user': n,
-            'rank': nodes_rank[n]['rank'],
-            'raw_rank': nodes_rank[n]['raw_rank'],
-            'timestamp': int(time.time() * 1000)
-        })
 
+    db = ArangoClient().db('_system')
+    counter = dict.fromkeys(range(0, 6), 0)
+    for node in ranker.graph:
+        counter[node.rank] += 1
+        db['verifications'].insert({
+            'name': 'Yekta',
+            'user': node.name,
+            'rank': node.rank,
+            'raw_rank': node.raw_rank,
+            'timestamp': int(time.time() * 1000),
+            'block': current_block
+        })
     print(f'verifieds: {counter}\n')
