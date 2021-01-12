5ae92858d8f614eb3efd3bf129225dbfd37a8eea
siftal <m.khanmohamadzadeh@gmail.com>
2021-01-12 07:35:52 -0500

    revoking verification of the users that are not eligible anymore

--- scorer/verifications/brightid.py
+++ scorer/verifications/brightid.py
@@ -8,21 +8,35 @@ def verify(block):
     print('BRIGHTID')
     db = ArangoClient(hosts=config.ARANGO_SERVER).db('_system')
     snapshot_db = ArangoClient(hosts=config.ARANGO_SERVER).db('snapshot')
-    already_verifieds = {v['user']
-                         for v in db['verifications'].find({'name': 'BrightID'})}
-    candidates = list(snapshot_db['verifications'].find(
-        {'name': 'SeedConnected'}))
+    already_verifieds = {v['user'] for v in db['verifications'].find(
+        {'name': 'BrightID'})}
 
-    for candidate in candidates:
-        if candidate['user'] in already_verifieds or candidate['rank'] < 1:
+    eligibles = snapshot_db.aql.execute('''
+        FOR v in verifications
+            FILTER v.name == 'SeedConnected'
+                AND v.rank > 0
+            RETURN v.user
+    ''')
+
+    for eligible in eligibles:
+        if eligible in already_verifieds:
+            already_verifieds.discard(eligible)
             continue
 
         db['verifications'].insert({
             'name': 'BrightID',
-            'user': candidate['user'],
+            'user': eligible,
             'block': block,
             'timestamp': int(time.time() * 1000),
-            'hash': utils.hash('BrightID', candidate['user'])
+            'hash': utils.hash('BrightID', eligible)
         })
+
+    # revoking verification of the users that are not eligible anymore
+    for ineligible in already_verifieds:
+        db['verifications'].delete_match({
+            'name': 'BrightID',
+            'user': ineligible
+        })
+
     verifiedCount = db['verifications'].find({'name': 'BrightID'}).count()
     print('verifieds: {}\n'.format(verifiedCount))
