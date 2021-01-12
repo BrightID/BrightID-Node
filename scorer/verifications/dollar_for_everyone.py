b989ca3696f6a24dc4712983d99c4f51cbcdfbdb
siftal <m.khanmohamadzadeh@gmail.com>
2021-01-12 07:36:52 -0500

    revoking verification of the users that are not eligible anymore

--- scorer/verifications/dollar_for_everyone.py
+++ scorer/verifications/dollar_for_everyone.py
@@ -10,8 +10,9 @@ def verify(block):
     print('DOLLAR FOR EVERYONE')
     db = ArangoClient(hosts=config.ARANGO_SERVER).db('_system')
     snapshot_db = ArangoClient(hosts=config.ARANGO_SERVER).db('snapshot')
+
     admins = [u['_id'] for u in snapshot_db['users'].find({'dfeAdmin': True})]
-    cursor = snapshot_db.aql.execute('''
+    eligibles = snapshot_db.aql.execute('''
         FOR c IN connections
             FILTER c._from IN @admins
                 AND c.level IN @levels
@@ -22,20 +23,29 @@ def verify(block):
         'levels': SEED_CONNECTION_LEVELS,
         'time_limit': 1564600000000
     })
-    users = {u.replace('users/', '') for u in cursor}
-    verifieds = {v['user']
-                 for v in db['verifications'].find({'name': 'DollarForEveryone'})}
-    for user in users:
-        if user in verifieds:
+    already_verifieds = {v['user'] for v in db['verifications'].find({
+        'name': 'DollarForEveryone'})}
+    for eligible in eligibles:
+        eligible = eligible.replace('users/', '')
+        if eligible in already_verifieds:
+            already_verifieds.discard(eligible)
             continue
 
         db['verifications'].insert({
             'name': 'DollarForEveryone',
-            'user': user,
+            'user': eligible,
             'block': block,
             'timestamp': int(time.time() * 1000),
-            'hash': utils.hash('DOLLAR FOR EVERYONE', 'user')
+            'hash': utils.hash('DollarForEveryone', eligible)
         })
+
+    # revoking verification of the users that are not eligible anymore
+    for ineligible in already_verifieds:
+        db['verifications'].delete_match({
+            'name': 'DollarForEveryone',
+            'user': ineligible
+        })
+
     verifiedCount = db['verifications'].find(
         {'name': 'DollarForEveryone'}).count()
     print('verifieds: {}\n'.format(verifiedCount))
