db0229537f45cc5989c7e61cf7b092bab239f9f6
siftal <m.khanmohamadzadeh@gmail.com>
2021-01-14 02:15:42 -0500

    use batch execution to improve performance

--- scorer/runner.py
+++ scorer/runner.py
@@ -36,7 +36,6 @@ def process(block):
 
 
 def main():
-    variables = db.collection('variables')
     while True:
         snapshots = [fname for fname in os.listdir(
             config.SNAPSHOTS_PATH) if fname.endswith('_fnl')]
@@ -64,22 +63,22 @@ def main():
 
 
 def update_apps_verification(block):
-    all_verifications = {}
-    for v in db['verifications'].find({'block': block}):
-        if v['user'] not in all_verifications:
-            all_verifications[v['user']] = {}
-
-        all_verifications[v['user']][v['name']] = True
-        for k in v:
-            if k in ['_key', '_id', '_rev', 'user', 'name']:
-                continue
-            all_verifications[v['user']][f'{v["name"]}.{k}'] = v[k]
-
     print('Check the users are verified for the apps')
     parser = Parser()
     apps = {app["_key"]: app['verification']
             for app in db['apps'] if app.get('verification')}
-    for user, verifications in all_verifications.items():
+    batch_db = db.begin_batch_execution(return_result=True)
+    batch_col = batch_db.collection('verifications')
+    counter = 0
+    for user in db['users']:
+        verifications = {}
+        for v in db['verifications'].find({'block': block, 'user': user['_key']}):
+            verifications[v['name']] = True
+            for k in v:
+                if k in ['_key', '_id', '_rev', 'user', 'name']:
+                    continue
+                verifications[f'{v["name"]}.{k}'] = v[k]
+
         for app in apps:
             try:
                 expr = parser.parse(apps[app])
@@ -91,13 +90,20 @@ def update_apps_verification(block):
                 print('invalid verification expression')
                 continue
             if verified:
-                db['verifications'].insert({
+                batch_col.insert({
                     'app': True,
                     'name': app,
                     'user': user,
                     'block': block,
                     'timestamp': int(time.time() * 1000)
                 })
+                counter += 1
+                if counter % 1000 == 0:
+                    batch_db.commit()
+                    batch_db = db.begin_batch_execution(return_result=True)
+                    batch_col = batch_db.collection('verifications')
+    if counter % 1000 != 0:
+        batch_db.commit()
 
 
 def update_verifications_state(block):
