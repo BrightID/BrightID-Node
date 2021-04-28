4fb2004c67e7b9f1b0b94b9486410b426f643710
Abram Symons <abramsymons@gmail.com>
2021-04-28 12:22:43 +0430

    remove old snapshots when node is re-initialized

--- scorer/config.py
+++ scorer/config.py
@@ -1,6 +1,11 @@
 import os
 
 SNAPSHOTS_PATH = "/snapshots"
+if 'INIT_BRIGHTID_DB' in os.environ:
+    for fname in os.listdir(SNAPSHOTS_PATH):
+        fpath = os.path.join(SNAPSHOTS_PATH, fname)
+        os.remove(fpath)
+
 BN_ARANGO_PROTOCOL = os.environ['BN_ARANGO_PROTOCOL']
 BN_ARANGO_HOST = os.environ['BN_ARANGO_HOST']
 BN_ARANGO_PORT = int(os.environ['BN_ARANGO_PORT'])
