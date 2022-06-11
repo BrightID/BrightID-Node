b200813ff33003b6622346caad2f6527a84989ca
siftal <m.khanmohamadzadeh@gmail.com>
2022-06-11 05:16:26 -0700

    format the codes

--- consensus/sender.py
+++ consensus/sender.py
@@ -1,15 +1,14 @@
-import os
 import socket
 import time
 import json
 import binascii
-import copy
 from arango import ArangoClient
 from web3 import Web3
 import config
 
 w3 = Web3(Web3.WebsocketProvider(config.INFURA_URL))
 db = ArangoClient(hosts=config.ARANGO_SERVER).db('_system')
+
 
 def sendTransaction(data):
     nonce = w3.eth.getTransactionCount(config.ADDRESS, 'pending')
@@ -25,6 +24,7 @@ def sendTransaction(data):
     signed = w3.eth.account.sign_transaction(tx, config.PRIVATE_KEY)
     tx = w3.eth.sendRawTransaction(signed.rawTransaction).hex()
     return tx
+
 
 def main():
     operations = []
@@ -42,16 +42,19 @@ def main():
         return
 
     data = json.dumps(operations).encode('utf-8')
-    data = '0x'+binascii.hexlify(data).decode('utf-8')
+    data = '0x' + binascii.hexlify(data).decode('utf-8')
     sendTransaction(data)
     for i, op in enumerate(operations):
-        db.collection('operations').update({'_key': hashes[i], 'state': 'sent'}, merge=True)
+        db.collection('operations').update(
+            {'_key': hashes[i], 'state': 'sent'}, merge=True)
+
 
 def wait():
     while True:
         time.sleep(5)
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        result = sock.connect_ex((config.BN_ARANGO_HOST, config.BN_ARANGO_PORT))
+        result = sock.connect_ex(
+            (config.BN_ARANGO_HOST, config.BN_ARANGO_PORT))
         sock.close()
         if result != 0:
             print('db is not running yet')
@@ -67,6 +70,7 @@ def wait():
             print('operations collection is not created yet')
             continue
         return
+
 
 if __name__ == '__main__':
     print('waiting for db ...')
