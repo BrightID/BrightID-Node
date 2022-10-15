2b8d719256076d22567dccadfad0eb78e2e24430
Adam Stallard <205792+adamstallard@users.noreply.github.com>
2022-10-15 00:44:14 -0700

    Make profile service run locally
    so that the profile service won't be exposed to the outside if people don't use a firewall.

--- web_services/profile/server.js
+++ web_services/profile/server.js
@@ -1,6 +1,6 @@
 const config = require("./config");
 const app = require("./app");
 
-app.listen(config.port, () => {
+app.listen(config.port, 'localhost', () => {
     console.log("Listening on port: ", config.port);
 });
