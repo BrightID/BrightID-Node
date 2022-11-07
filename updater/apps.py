656cd23801f9212408e82d6b1620789d0581bd0b
Siftal <m.khanmohamadzadeh@gmail.com>
2022-11-07 03:23:07 -0500

    fix and issue, check app by marshmallow and improve performance

--- updater/apps.py
+++ updater/apps.py
@@ -6,10 +6,23 @@ from web3 import Web3
 from arango import ArangoClient
 from web3.middleware import geth_poa_middleware
 import config
-
+from marshmallow import Schema, fields
 
 db = ArangoClient(hosts=config.ARANGO_SERVER).db('_system')
-local_to_json = {
+w3_mainnet = Web3(Web3.WebsocketProvider(
+    config.MAINNET_WSS, websocket_kwargs={'timeout': 60}))
+sp_contract_mainnet = w3_mainnet.eth.contract(
+    address=config.MAINNET_SP_ADDRESS,
+    abi=config.SP_ABI)
+
+w3_idchain = Web3(Web3.WebsocketProvider(
+    config.IDCHAIN_WSS, websocket_kwargs={'timeout': 60}))
+w3_idchain.middleware_onion.inject(geth_poa_middleware, layer=0)
+sp_contract_idchain = w3_idchain.eth.contract(
+    address=config.IDCHAIN_SP_ADDRESS,
+    abi=config.SP_ABI)
+
+key_converter_dic = {
     '_key': 'Key',
     'name': 'Name',
     'context': 'Context',
@@ -26,8 +39,37 @@ local_to_json = {
     'soulbound': 'Soulbound',
     'callbackUrl': 'Callback Url',
     'poaNetwork': 'POA Network',
-    'rpcEndpoint': 'RPC Endpoint'
+    'rpcEndpoint': 'RPC Endpoint',
+    'callbackUrl': 'Callback Url'
 }
+
+
+class AppSchema(Schema):
+    _key = fields.String(required=True, allow_none=True)
+    name = fields.String(required=True, allow_none=True)
+    context = fields.String(required=True, allow_none=True)
+    sponsorPublicKey = fields.String(required=True, allow_none=True)
+    sponsorEventContract = fields.String(required=True, allow_none=True)
+    verification = fields.String(required=True, allow_none=True)
+    verifications = fields.List(
+        fields.String(), metadata={'allow_blank': True})
+    testing = fields.Boolean(required=True)
+    idsAsHex = fields.Boolean(required=True)
+    usingBlindSig = fields.Boolean(required=True)
+    localFilter = fields.Boolean(required=True)
+    nodeUrl = fields.URL(required=True, allow_none=True)
+    verificationExpirationLength = fields.Integer(
+        required=True, allow_none=True)
+    soulbound = fields.Boolean(required=True)
+    poaNetwork = fields.Boolean(required=True)
+    rpcEndpoint = fields.URL(required=True, allow_none=True, schemes={
+                             'http', 'https', 'ws', 'wss'})
+    callbackUrl = fields.URL(required=True, allow_none=True)
+    url = fields.URL(required=True, allow_none=True)
+    logo = fields.String(required=True, allow_none=True)
+
+
+app_schema = AppSchema()
 
 
 def str2bytes32(s):
@@ -50,87 +92,67 @@ def get_logo(url):
     return logo
 
 
-def apps_data():
-    print('Updating apps', time.ctime())
-    local_apps = {app['_key']: app for app in db['apps']}
-
-    data = requests.get(config.APPS_JSON_FILE).json()
-
-    new_local_apps = []
-    for json_app in data['Applications']:
-        new_local_app = {key: json_app[local_to_json[key]]
-                         for key in local_to_json if local_to_json[key] in json_app}
-        if 'verificationExpirationLength' in new_local_app:
-            new_local_app['verificationExpirationLength'] = int(
-                new_local_app['verificationExpirationLength']) if new_local_app['verificationExpirationLength'].isdigit() else 0
-        if 'Links' in json_app:
-            new_local_app['url'] = next(iter(json_app['Links'] or []), '')
-        if 'Images' in json_app:
-            new_local_app['logo'] = get_logo(
-                next(iter(json_app['Images'] or []), ''))
-        local_app = local_apps.get(json_app['Key'])
-        if not local_app:
-            print(f"New app: {new_local_app['_key']}")
-            new_local_apps.append(new_local_app)
-            continue
-
-        for key in new_local_app:
-            if new_local_app.get(key) != local_app.get(key):
-                print(f"Updating {new_local_app['_key']} app")
-                try:
-                    db['apps'].update(new_local_app)
-                except Exception as e:
-                    print(f'Error in updating app: {e}')
-                break
-    try:
-        print("Inserting new apps")
-        db['apps'].import_bulk(new_local_apps)
-    except Exception as e:
-        print(f'Error in inserting new apps: {e}')
-
-    for app_key in data['Removed apps']:
-        if local_apps.get(app_key):
-            try:
-                print(f"Removing {app_key} app")
-                db['apps'].delete(app_key)
-            except Exception as e:
-                print(f'Error in removing app: {e}')
-
-
-def apps_balance():
-    print("Updating sponsorships balance of the apps", time.ctime())
-    w3_mainnet = Web3(Web3.WebsocketProvider(
-        config.MAINNET_WSS, websocket_kwargs={'timeout': 60}))
-    sp_contract_mainnet = w3_mainnet.eth.contract(
-        address=config.MAINNET_SP_ADDRESS,
-        abi=config.SP_ABI)
-
-    w3_idchain = Web3(Web3.WebsocketProvider(
-        config.IDCHAIN_WSS, websocket_kwargs={'timeout': 60}))
-    w3_idchain.middleware_onion.inject(geth_poa_middleware, layer=0)
-    sp_contract_idchain = w3_idchain.eth.contract(
-        address=config.IDCHAIN_SP_ADDRESS,
-        abi=config.SP_ABI)
-
-    for app in db['apps']:
-        app_bytes = str2bytes32(app['_key'])
-        mainnet_balance = sp_contract_mainnet.functions.totalContextBalance(
-            app_bytes).call()
-        idchain_balance = sp_contract_idchain.functions.totalContextBalance(
-            app_bytes).call()
-        app['totalSponsorships'] = mainnet_balance + idchain_balance
-        print(app['_key'], app['totalSponsorships'])
-        db['apps'].update(app)
+def row_to_app(row):
+    app = {k1: row[k2] if row[k2] != '' else None for k1,
+           k2 in key_converter_dic.items() if k2 in row}
+    app['url'] = next(iter(row.get('Links', [])), '').strip()
+    app['logo'] = get_logo(next(iter(row.get('Images', [])), '').strip())
+    app = app_schema.load(app)
+    return app
 
 
 def update():
-    apps_data()
-    apps_balance()
+    print('Updating apps', time.ctime())
+    data = requests.get(config.APPS_JSON_FILE).json()
+    for row in data['Applications']:
+        try:
+            app = row_to_app(row)
+        except Exception as e:
+            print(f'app: {row["Key"]} => Invalid data: {e}')
+            app = {'_key': row["Key"]}
+
+        try:
+            app['totalSponsorships'] = get_sponsorships(app['_key'])
+            # print(f'app: {app["_key"]} => totalSponsorships: {app["totalSponsorships"]}')
+        except Exception as e:
+            print(f'app: {row["Key"]} => Error in get totalSponsorships: {e}')
+
+        if not app:
+            continue
+
+        db.aql.execute('''
+            INSERT @app IN apps
+            OPTIONS { overwriteMode: "update" }
+            RETURN OLD
+        ''', bind_vars={
+            "app": app
+        })
+
+    for app_key in data['Removed apps']:
+        db.aql.execute('''
+            for app in apps
+                filter app._key == @key
+                REMOVE { _key: app._key } IN apps OPTIONS { ignoreErrors: true }
+        ''', bind_vars={
+            'key': app_key,
+        })
+
+
+def get_sponsorships(app_key):
+    app_bytes = str2bytes32(app_key)
+    mainnet_balance = sp_contract_mainnet.functions.totalContextBalance(
+        app_bytes).call()
+    idchain_balance = sp_contract_idchain.functions.totalContextBalance(
+        app_bytes).call()
+    totalSponsorships = mainnet_balance + idchain_balance
+    return totalSponsorships
 
 
 if __name__ == '__main__':
     try:
+        ts = time.time()
         update()
+        print(f'Updating apps ended in {int(time.time() - ts)} seconds')
     except Exception as e:
         print(f'Error in updater: {e}')
         traceback.print_exc()
