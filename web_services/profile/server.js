020853bc78af80558382cbf50b23ae36964237c5
Michael Bauer <michael@m-bauer.org>
2022-11-30 13:48:50 +0100

    Don't restrict profile service to localhost only, this breaks the dappnode package :-/

--- web_services/profile/server.js
+++ web_services/profile/server.js
@@ -1,6 +1,6 @@
 const config = require("./config");
 const app = require("./app");
 
-app.listen(config.port, 'localhost', () => {
+app.listen(config.port, () => {
     console.log("Listening on port: ", config.port);
 });
